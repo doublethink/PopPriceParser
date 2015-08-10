a516a1b4ccaa3add8cbf42e41856479d148d8874
stephen <stephen@doublethink.co.nz>
2015-08-10 15:05:00 +1200

    added single daily table which inserts into bulk table

--- PopBot.py
+++ PopBot.py
@@ -23,13 +23,29 @@ date = date.isoformat()
 connection = None
 
 try:
-	connection = psycopg2.connect(database='poppricedb', user='stephen')
-	cursor = connection.cursor()
-	for pops in results:
-		for pop in pops:
-			cursor.execute('INSERT INTO raw_daily_rates VALUES (\'{0}\', \'{1}\', \'{2}\', {3}, {4}, \'{5}\', \'{6}\');'.format(pop.category, pop.number, pop.name.replace("'",""), pop.value, pop.is_variant, pop.url, date))
-	
-	connection.commit()
+	# Connect to internal Postgres server
+    connection = psycopg2.connect(database='poppricedb', user='stephen')
+    cursor = connection.cursor()
+    # Set up todays summary
+    cursor.execute('DROP TABLE IF EXISTS raw_single_day;')
+    statement = """CREATE TABLE raw_single_day (
+        category varchar(15),
+        number varchar(15),
+        name text,
+        value money,
+        is_variant boolean,
+        url text,
+        date text
+    );"""
+    cursor.execute(statement)
+    # Insert results into daily table
+    for pops in results:
+        for pop in pops:
+            cursor.execute('INSERT INTO raw_single_day VALUES (\'{0}\', \'{1}\', \'{2}\', {3}, {4}, \'{5}\', \'{6}\');'.format(pop.category, pop.number, pop.name.replace("'",""), pop.value, pop.is_variant, pop.url, date))
+
+    # Insert the remaining into the summary page
+    cursor.execute('INSERT INTO raw_daily_rates SELECT * FROM raw_single_day ;')
+    connection.commit()
 
 except Exception as e:
 	logging.debug("Error {0}".format(e))
